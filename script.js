66b4501a80928c6cd2f735705a80fe4df16b54d5
Ante Dragoje <antekrtalic2@gmail.com>
2021-05-29 21:15:13 +0200

    Almost done with my mini project, fixing Prev button

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const nums = document.querySelectorAll('.circle');
 const next_btn = document.getElementById('next');
 const prev_btn = document.getElementById('prev');
-let i = 1;
+let i= 1;
+let j = nums.length - 1;
 
-if (nums[nums.length - 1].classList.contains('active')) {
-    next_btn.disabled = true;
-} else if (!nums[i].classList.contains('active') && (!nums[nums.length - 1].classList.contains('active'))) {
-    next_btn.addEventListener('click', () => {
-        nums[i].classList.add('active');
-        i += 1;
-    })    
-}
-
+next_btn.addEventListener('click', () => {
+    nums[i].classList.add('active');
+    i += 1;
+    if (nums[nums.length - 1].classList.contains('active')) {
+        next_btn.disabled = true;
+        next_btn.classList.remove('active');
+        prev_btn.disabled = false;
+        prev_btn.classList.add('active');
+    }
+})
+console.log(i);
