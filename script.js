a16998331983ec976f16c05e90ec10fa54280f1b
Ante Dragoje <antekrtalic2@gmail.com>
2021-05-29 20:39:45 +0200

    Trying to fix my errors for adding classes to element using js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const nums = document.querySelectorAll('.circle');
-const next_btn = document.querySelector('#next');
-const prev_btn = document.querySelector('#prev');
+const next_btn = document.getElementById('next');
+const prev_btn = document.getElementById('prev');
+let i = 1;
 
-nums.forEach(element => {
-    element.addEventListener('click', () => {
-        
-    })
-
-})
+if (nums[nums.length - 1].classList.contains('active')) {
+    next_btn.disabled = true;
+} else if (!nums[i].classList.contains('active') && (!nums[nums.length - 1].classList.contains('active'))) {
+    next_btn.addEventListener('click', () => {
+        nums[i].classList.add('active');
+        i += 1;
+    })    
+}
 
