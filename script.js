c47938e36d9a9dd61fe06a5fe6ba6156d2f2d293
Ante Dragoje <antekrtalic2@gmail.com>
2021-05-30 16:57:14 +0200

    Trying to add modification for line charging

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const nums = document.querySelectorAll('.circle');
+const spans = document.querySelectorAll('.line');
 const next_btn = document.getElementById('next');
 const prev_btn = document.getElementById('prev');
 let i= 1;
 let j = nums.length - 1;
+console.log(spans.length);
 
 next_btn.addEventListener('click', () => {
-    nums[i].classList.add('active');
+    nums[i].classList.add('active'); 
+    spans[i - 1].classList.add('active');
     i += 1;
     if (nums[nums.length - 1].classList.contains('active')) {
         next_btn.disabled = true;
@@ -14,4 +17,18 @@ next_btn.addEventListener('click', () => {
         prev_btn.classList.add('active');
     }
 })
-console.log(i);
+
+
+prev_btn.addEventListener('click', () => {
+    console.log(i);
+    i -= 1;
+    nums[i].classList.remove('active');
+    
+    if (i === 1) {
+        prev_btn.disabled = true;
+        prev_btn.classList.remove('active');
+        next_btn.disabled = false;
+        next_btn.classList.add('active');
+    }
+})
+
